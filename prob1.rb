af9eb66e21c9537dd351d9745e156695330a92b3
Naimah Muhammad <naimahmuhammad07@gmail.com>
2024-03-26 21:40:52 -0500

    Solution to problem 1

---
+++ prob1.rb
@@ -0,0 +1,24 @@
+require 'httparty'
+
+response = HTTParty.get('https://api.github.com/users/CherryMae1/repos')
+data = JSON.parse(response.body)
+
+def most_starred_repo(data)
+  max_star = data.max{ |x| x['stargazers_count']}
+  {
+    name: max_star['name'],
+    description: max_star['description'],
+    star_count: max_star['stargazers_count'],
+    repo_url: max_star['html_url'],
+  }
+end
+
+def display_repo(data)
+  puts "Name: #{data[:name]}"
+  puts "Description: #{data[:description]}"
+  puts "Count Stars: #{data[:star_count]}"
+  puts "Repo URL: #{data[:repo_url]}"
+end
+
+x = most_starred_repo(data)
+y = (display_repo(x))
